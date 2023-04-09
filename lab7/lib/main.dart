498164f546c6eaef0d9073d0ea8b19d413b52ca5
Lobanov Nikolay <nicklob4nov@yandex.ru>
2023-04-09 13:04:34 +0300

    создан класс для страницы

--- lab7/lib/main.dart
+++ lab7/lib/main.dart
@@ -30,9 +30,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -41,17 +38,9 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
       ),
       body: Center(
-
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
           ],
         ),
       ),
