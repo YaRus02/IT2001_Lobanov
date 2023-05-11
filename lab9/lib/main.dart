bbb4f390fa192223691386df8c6dc3ee2c817418
Lobanov Nikolay <nicklob4nov@yandex.ru>
2023-05-12 01:00:31 +0300

    добавлены тесты

--- lab9/lib/main.dart
+++ lab9/lib/main.dart
@@ -1,5 +1,14 @@
 import 'package:lab9/Classes/Machine.dart';
 
 void main() {
-
+  CoffeeMachine test = CoffeeMachine(100, 100, 100, 100);
+  print(test.coffeeBeans);
+  print(test.milk);
+  print(test.water);
+  print(test.cash);
+  test.makingCoffee();
+  print(test.coffeeBeans);
+  print(test.milk);
+  print(test.water);
+  print(test.cash);
 }
