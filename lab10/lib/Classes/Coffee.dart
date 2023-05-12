25cc911ee2bdc6120102efb3aaf8b83b1dc02a8b
Lobanov Nikolay <nicklob4nov@yandex.ru>
2023-05-12 15:38:48 +0300

    создааны остальные классы по типам коффе, добавились эспрессо и американо

--- lab10/lib/Classes/Coffee.dart
+++ lab10/lib/Classes/Coffee.dart
@@ -34,3 +34,70 @@ class Cappuccino implements ICoffee {
     return _resources;
   }
 }
+
+class Espresso implements ICoffee {
+  int _coffeeBeans = 0;
+  int _water = 0;
+  int _milk = 0;
+  int _cash = 0;
+
+  final Resources _resources = Resources( 30, 50, 100, 150);
+
+  CoffeeTypes get type {
+    return CoffeeTypes.cappuccino;
+  }
+
+  int get coffeBeansRequired {
+    return _coffeeBeans = _resources.cash;
+  }
+
+  int get waterRequired {
+    return _water = _resources.cash;
+  }
+
+  int get milkRequired {
+    return _milk = _resources.cash;
+  }
+
+  int get coffePrice {
+    return _cash = _resources.cash;
+  }
+
+  Resources checkResources() {
+    return _resources;
+  }
+}
+
+class Americano implements ICoffee {
+  int _coffeeBeans = 0;
+  int _water = 0;
+  int _milk = 0;
+  int _cash = 0;
+
+  final Resources _resources = Resources( 30, 50, 100, 150);
+
+  CoffeeTypes get type {
+    return CoffeeTypes.cappuccino;
+  }
+
+  int get coffeBeansRequired {
+    return _coffeeBeans = _resources.cash;
+  }
+
+  int get waterRequired {
+    return _water = _resources.cash;
+  }
+
+  int get milkRequired {
+    return _milk = _resources.cash;
+  }
+
+  int get coffePrice {
+    return _cash = _resources.cash;
+  }
+
+  Resources checkResources() {
+    return _resources;
+  }
+}
+
