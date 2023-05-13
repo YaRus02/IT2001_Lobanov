169c29910e2618a3078725f2865b23c827341a40
Lobanov Nikolay <nicklob4nov@yandex.ru>
2023-05-13 12:38:47 +0300

    в файл асинк функций добавлены сами функции + тесты к ним

---
+++ lab11/lib/Classes/AsyncCoffee.dart
@@ -0,0 +1,20 @@
+import 'package:lab11/Classes/Coffee_inter.dart';
+
+Future<void> bwater () {
+  print('start process bwater');
+  return Future.delayed(Duration(seconds: 5)).then((_) => print('end process bwater'));
+}
+
+Future<void> bcoffee (ICoffee coffee) {
+  print('start process bcoffe for ${coffee.coffeeName}');
+  if(coffee.milkRequired == 0) {
+    return Future.delayed(Duration(seconds: 3)).then((_) => print('end process bcoffee for ${coffee.coffeeName}'));
+  }else {
+    return Future.delayed(Duration(seconds: 3)).then((_) => print('end process bcoffee & whippingMilk for ${coffee.coffeeName}'));
+  }
+}
+
+Future<void> mixmilkWcoffee () {
+  print('start process mixmilkWcoffee');
+  return Future.delayed(Duration(seconds: 5)).then((_) => print('end process mixmilkWcoffee'));
+}
--- lab11/lib/main.dart
+++ lab11/lib/main.dart
@@ -1,44 +1,33 @@
+import 'package:lab11/Classes/AsyncCoffee.dart';
+
 import 'Classes/Coffee.dart';
 import 'Classes/Machine.dart';
 import 'Classes/Resources.dart';
 
-Future<void> test () async {
-  print('start process');
-  await Future.delayed(Duration(seconds: 5)).then((_) => print('end process'));
-}
-
 void main () async {
-  CoffeeMachine machine = CoffeeMachine(Resources(250,250,250,250));
+  CoffeeMachine machine = CoffeeMachine(Resources(450,450,450,450));
   print('кофейные зерна: ${machine.resources.coffeeBeans}');
   print('молоко: ${machine.resources.milk}');
   print('вода: ${machine.resources.water}');
   print('деньжата: ${machine.resources.cash}');
-
-  print(machine.resources.cash);
 
   Cappuccino cappuccino = Cappuccino();
   Espresso espresso = Espresso();
   Americano americano = Americano();
 
-  machine.makeCoffee(cappuccino);
-  await test();
-  print('кофейные зерна: ${machine.resources.coffeeBeans}');
-  print('молоко: ${machine.resources.milk}');
-  print('вода: ${machine.resources.water}');
-  print('деньжата: ${machine.resources.cash}');
+  //machine.makeCoffee(cappuccino);
+  await bwater();
+  await bcoffee(cappuccino);
+  await mixmilkWcoffee();
 
-  machine.makeCoffee(espresso);
-  await test();
-  print('кофейные зерна: ${machine.resources.coffeeBeans}');
-  print('молоко: ${machine.resources.milk}');
-  print('вода: ${machine.resources.water}');
-  print('деньжата: ${machine.resources.cash}');
+ // machine.makeCoffee(espresso);
+  await bwater();
+  await bcoffee(espresso);
+  await mixmilkWcoffee();
 
-  machine.makeCoffee(americano);
-  await test();
-  print('кофейные зерна: ${machine.resources.coffeeBeans}');
-  print('молоко: ${machine.resources.milk}');
-  print('вода: ${machine.resources.water}');
-  print('деньжата: ${machine.resources.cash}');
+  //machine.makeCoffee(americano);
+  await bwater();
+  await bcoffee(americano);
+  await mixmilkWcoffee();
 
 }
