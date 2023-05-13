b52bdba6817f86d8e07dee5dcec0eb706188d239
Lobanov Nikolay <nicklob4nov@yandex.ru>
2023-05-13 11:16:26 +0300

    создан файл для асинк функцийй

--- lab11/lib/main.dart
+++ lab11/lib/main.dart
@@ -1,3 +1,36 @@
-void main() {
+import 'Classes/Coffee.dart';
+import 'Classes/Machine.dart';
+import 'Classes/Resources.dart';
+
+void main () {
+  CoffeeMachine machine = CoffeeMachine(Resources(250,250,250,250));
+  print('кофейные зерна: ${machine.resources.coffeeBeans}');
+  print('молоко: ${machine.resources.milk}');
+  print('вода: ${machine.resources.water}');
+  print('деньжата: ${machine.resources.cash}');
+
+  print(machine.resources.cash);
+
+  Cappuccino cappuccino = Cappuccino();
+  Espresso espresso = Espresso();
+  Americano americano = Americano();
+
+  machine.makeCoffee(cappuccino);
+  print('кофейные зерна: ${machine.resources.coffeeBeans}');
+  print('молоко: ${machine.resources.milk}');
+  print('вода: ${machine.resources.water}');
+  print('деньжата: ${machine.resources.cash}');
+
+  machine.makeCoffee(espresso);
+  print('кофейные зерна: ${machine.resources.coffeeBeans}');
+  print('молоко: ${machine.resources.milk}');
+  print('вода: ${machine.resources.water}');
+  print('деньжата: ${machine.resources.cash}');
+
+  machine.makeCoffee(americano);
+  print('кофейные зерна: ${machine.resources.coffeeBeans}');
+  print('молоко: ${machine.resources.milk}');
+  print('вода: ${machine.resources.water}');
+  print('деньжата: ${machine.resources.cash}');
 
 }
